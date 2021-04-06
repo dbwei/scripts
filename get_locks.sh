ac5bbaa7ed80d7c76b0d6e1b9560378630735717
dbwei <35121241+dbwei@users.noreply.github.com>
2021-04-06 09:30:39 +0800

    Create get_locks.sh

---
+++ get_locks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+<<!
+ **********************************************************
+ * Author        : lihaimao
+ * Email         : haimao_li@163.com
+ * Last modified : 2021-04-06 09:30
+ * Filename      : get_locks
+ * Description   :
+ * *******************************************************
+!
+user=lhm
+pass='123456'
+host=192.168.124.8
+port=3306
+logdir=/tmp/tdsql
+
+CONN="mysql -u$user -p$pass -h$host -P$port"
+
+count=0
+while [ $count -lt 5 ]; do
+
+locks=$(mysql -u$user -p$pass -h$host -P$port -Bse "show global status like 'Innodb_row_lock_current_waits';" | awk '{print $2}')
+echo $locks
+if [ $locks = 0 ]
+then 
+echo -e "$(date +%F' '%T)\n" >> $logdir/lock.log
+$CONN -Bse "select CONCAT('thread ', b.trx_mysql_thread_id, ' from ', p.host) AS who_blocks , r.trx_id waiting_trx_id , r.trx_mysql_thread_id waiting_thread , r.trx_query waiting_query , b.trx_id blocking_trx_id , b.trx_mysql_thread_id block_thread , b.trx_query blocking_query from information_schema.innodb_lock_waits w inner join information_schema.innodb_trx b on b.trx_id = w.blocking_trx_id inner join information_schema.innodb_trx r on r.trx_id = w.requesting_trx_id left join information_schema.processlist p on p.id = b.trx_mysql_thread_id;" >> $logdir/lock.log
+
+echo -e "$(date +%F' '%T)\n" >> $logdir/innodbstatus.log
+$CONN -Bse "show engine innodb status\G" >> $logdir/innodbstatus.log
+echo -e "$(date +%F' '%T)\n" >> $logdir/metadatalocks.log
+$CONN -Bse "select * from performance_schema.metadata_locks;" >> $logdir/metadatalocks.log
+echo -e "$(date +%F' '%T)\n" >> $logdir/processlist.log
+$CONN -Bse "select * from information_schema.processlist\G" >> $logdir/processlist.log
+let count=$count+1
+sleep 3;
+fi
+done
